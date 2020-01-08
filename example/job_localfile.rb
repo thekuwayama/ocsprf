08a26a9b645fb2abdbe598b8952aebef793d88e7
thekuwayama <thekuwayama@gmail.com>
2020-01-08 15:51:13 +0900

    clean example; rm unnecessary method

--- example/job_localfile.rb
+++ example/job_localfile.rb
@@ -4,30 +4,26 @@ require_relative 'helper'
 
 CACHE_FILE_PATH = '/tmp/ocsp_response.der'
 
-def fetch
-  read_local_file = lambda do
-    return nil unless File.exist?(CACHE_FILE_PATH)
+read_local_file = lambda do
+  return nil unless File.exist?(CACHE_FILE_PATH)
 
-    der = File.binread(CACHE_FILE_PATH)
-    ocsp_response = OpenSSL::OCSP::Response.new(der)
-    return nil if ocsp_response.basic.status.first[5] < Time.now
+  der = File.binread(CACHE_FILE_PATH)
+  ocsp_response = OpenSSL::OCSP::Response.new(der)
+  return nil if ocsp_response.basic.status.first[5] < Time.now
 
-    ocsp_response
-  end
-
-  write_local_file = lambda do |ocsp_response|
-    File.binwrite(CACHE_FILE_PATH, ocsp_response.to_der)
-  end
-
-  ee, inter = parse_options
-  fetcher = OCSPResponseFetcher.new(
-    OpenSSL::X509::Certificate.new(File.read(ee)),
-    OpenSSL::X509::Certificate.new(File.read(inter)),
-    nil,
-    read_local_file,
-    write_local_file
-  )
-  fetcher.run
+  ocsp_response
 end
 
-fetch
+write_local_file = lambda do |ocsp_response|
+  File.binwrite(CACHE_FILE_PATH, ocsp_response.to_der)
+end
+
+ee, inter = parse_options
+fetcher = OCSPResponseFetcher.new(
+  OpenSSL::X509::Certificate.new(File.read(ee)),
+  OpenSSL::X509::Certificate.new(File.read(inter)),
+  nil,
+  read_local_file,
+  write_local_file
+)
+fetcher.run
--- example/job_print.rb
+++ example/job_print.rb
@@ -2,13 +2,9 @@
 
 require_relative 'helper'
 
-def fetch
-  ee, inter = parse_options
-  fetcher = OCSPResponseFetcher.new(
-    OpenSSL::X509::Certificate.new(File.read(ee)),
-    OpenSSL::X509::Certificate.new(File.read(inter))
-  )
-  fetcher.run
-end
-
-fetch
+ee, inter = parse_options
+fetcher = OCSPResponseFetcher.new(
+  OpenSSL::X509::Certificate.new(File.read(ee)),
+  OpenSSL::X509::Certificate.new(File.read(inter))
+)
+fetcher.run
